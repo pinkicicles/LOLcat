50604f33639393c757322668019fecbf2ed014a3
Connie <wuconnie@seas.upenn.edu>
2013-01-11 03:21:05 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LOLcat::Application.config.secret_token = '5a8e6cfaa669d62aee36cd0bd69bd5f026aa159b6cc5274e4cf9878f3acaa82e09d028f703ad8c9fbf3c8e7710d17c87f2370162f7df3f697a7d61c9fab5d7d3'

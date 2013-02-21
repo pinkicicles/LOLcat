ecf73133ed28fe06d7bf955f83248d92044aff65
Connie <connie_p_wu@yahoo.com>
2013-02-20 21:08:18 -0500

    again

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130206043608) do
+ActiveRecord::Schema.define(:version => 20130220192939) do
 
   create_table "chefs", :force => true do |t|
     t.string   "name"
@@ -20,6 +20,28 @@ ActiveRecord::Schema.define(:version => 20130206043608) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
+
+  create_table "culinaries", :force => true do |t|
+    t.string   "Artist"
+    t.string   "Name"
+    t.text     "Dishes"
+    t.string   "Location"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "rails_admin_histories", :force => true do |t|
+    t.text     "message"
+    t.string   "username"
+    t.integer  "item"
+    t.string   "table"
+    t.integer  "month",      :limit => 2
+    t.integer  "year",       :limit => 5
+    t.datetime "created_at",              :null => false
+    t.datetime "updated_at",              :null => false
+  end
+
+  add_index "rails_admin_histories", ["item", "table", "month", "year"], :name => "index_rails_admin_histories"
 
   create_table "sponsors", :force => true do |t|
     t.string   "Name"

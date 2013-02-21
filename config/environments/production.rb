c705334ebb4603470d93c3aef0689b658c8dc71f
Connie <connie_p_wu@yahoo.com>
2013-02-20 21:09:44 -0500

    deleted weird thing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,5 +65,4 @@ LOLcat::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-  config.action_mailer.default_url_options { :host => 'hw01.herokuapp.com'}
 end

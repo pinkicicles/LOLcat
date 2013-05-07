4bda9a87f46f46ebf49c31b3310a403a0ab3aac0
Connie <connie_p_wu@yahoo.com>
2013-05-07 02:14:36 -0400

    models

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,3 +1,10 @@
 class Meal < ActiveRecord::Base
   attr_accessible :chef, :date, :description, :location, :name, :pickup_time
+
+  validates :date, presence: true
+
+  validates :name, presence: true
+
+  validates :chef, presence: true
+
 end
